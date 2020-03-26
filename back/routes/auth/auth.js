1849c8d9a527e20608acd7c7fc613c7b57a52f82
Andrea de Roeck <deroeck.andrea@gmail.com>
2020-03-26 22:36:37 +0000

    Create backend database js and init sql file

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,12 +1,32 @@
 const express = require("express");
 const router = express.Router();
+const connection = require("../../helpers/db");
 const bodyParser = require("body-parser");
 
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 
 router.post("/signup", function(req, res, next) {
-  res.send("I am in POST signup");
+  const user = {
+    email: req.body.email,
+    password: req.body.password,
+    name: req.body.name,
+    lastname: req.body.lastname
+  };
+  connection.query("INSERT INTO users SET ?", user, function(
+    error,
+    results,
+    fields
+  ) {
+    if (error) {
+      console.log(error);
+      res.status(500).end();
+      // res.status(500).send("Error in in login");
+    } else {
+      res.status(200).json(user);
+      res.end();
+    }
+  });
 });
 
 module.exports = router;
